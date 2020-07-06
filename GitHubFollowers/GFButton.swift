e88b353c42aa532612699ba09b9d80c2ace2032a
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-06 22:20:25 +0400

    Create Custom Reusable Objects - Button, TextField

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by KHAIRI on 06/07/2020.
+//  Copyright © 2020 KHAIRI. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Custom code
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
