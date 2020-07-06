247819215d0ac33461a65b9d9d7dfe57b540d4d0
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-06 12:12:50 +0400

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by KHAIRI on 06/07/2020.
+//  Copyright © 2020 KHAIRI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
