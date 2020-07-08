9e3ef226ddf5a5b700c835c97dbfd4304728494e
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-08 17:54:09 +0400

    Result type refactor

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -11,26 +11,28 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollwers(for: username, page: 1) { followers, errorMessage in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
-            }
+        NetworkManager.shared.getFollwers(for: username, page: 1) { result in
             
-            print("Followers.cout = \(followers.count)")
-            print(followers)
+            switch result {
+            case .success(let followers):
+                print("Followers.cout = \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happend", message: error.rawValue, buttonTitle: "Ok")
+            }   
         }
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-  
+    
 }
