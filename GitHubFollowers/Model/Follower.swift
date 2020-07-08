edc3ffdebfb29e1f268b114a44da11cccdf9912c
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-08 06:55:31 +0400

    Add Follower Model

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by KHAIRI on 08/07/2020.
+//  Copyright © 2020 KHAIRI. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
