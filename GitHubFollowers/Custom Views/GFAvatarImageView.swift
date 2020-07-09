f478b1e360be27000f77aa4ca017ad1078d206e6
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-09 11:40:39 +0400

    Add Custom avatar image view

---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by KHAIRI on 09/07/2020.
+//  Copyright © 2020 KHAIRI. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds      = true
+        image              = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
