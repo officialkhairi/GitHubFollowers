6f9a1737b9c8f650578dde4ad7177ff6c516f16e
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-10 23:26:17 +0400

    Downloading images asynchronously and cache each image using NSCache

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -6,11 +6,13 @@
 //  Copyright © 2020 KHAIRI. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
+    
     private init(){}
     
     func getFollwers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
