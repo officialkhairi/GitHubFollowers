879642319a17b80f177d2dcb486c4a0b1995613d
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-13 23:59:35 +0400

    Add userInfo VC

---
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,19 @@
+//
+//  UserInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by KHAIRI on 13/07/2020.
+//  Copyright © 2020 KHAIRI. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
