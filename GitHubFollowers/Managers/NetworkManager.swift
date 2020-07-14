d3c87d15937845347b2732bbcc5c4c7d438bc07e
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-14 11:29:31 +0400

    Add network call to bring user info

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -53,4 +53,43 @@ class NetworkManager {
         task.resume()
         
     }
+    
+    func getUserInfo(for username: String, completed: @escaping (Result<User, GFError>) -> Void) {
+        let endpint = baseURL + "\(username)"
+        
+        guard let url = URL(string: endpint) else {
+            completed(.failure(.invalidUserName))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
 }
--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -19,7 +19,16 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
         
-        print(username!)
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something wrong went", message: error.rawValue, buttonTitle: "ok")
+            }
+        }
     }
     
     @objc func dismissVC(){
