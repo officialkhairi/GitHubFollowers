555794e0edcc505f7d073dac0da416c4a29be817
Ahmed Khairi <officialkhairi@gmail.com>
2020-07-19 21:22:22 +0400

    Refactor Safari View Controller

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import SafariServices
 
 protocol UserInfoVCDelegate: class {
     func didTapGithubProfile(for user: User)
@@ -122,9 +121,7 @@ extension UserInfoVC: UserInfoVCDelegate {
             return
         }
         
-        let safariVC = SFSafariViewController(url: url)
-        safariVC.preferredControlTintColor = .systemGreen
-        present(safariVC, animated: true)
+        presentSafariVC(with: url)
         
     }
     
